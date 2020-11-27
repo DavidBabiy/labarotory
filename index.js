d85624f63cf16cdf3e0b86a6a5954568756ea3fa
David <david.babiy1995@gmail.com>
2020-11-27 20:00:39 +0200

    laba 8

--- index.js
+++ index.js
@@ -1,77 +1,40 @@
-let xList = [0, 1, 2 ,3 ,4];
-let fxList = [5.29, 2.31, 0.01, 1.69, 5.72];
+let a = -2.;
+let b = 1.;
+let e = .005;
 
-for (let x of xList) {
+let x = .0;
+let s = 0;
 
-    console.log("f(" + x + ") = " + eitken(x, xList.length, xList, fxList));
-}
-for (let x of xList) {
-    console.log("f(" + x + ") = " + laGrange(x, xList.length, xList, fxList));
-}
-for (let x of xList) {
-    console.log("f(" + x + ") = " + splain(x, xList.length, xList, fxList));
-    console.log("Inaccuracy = " + inaccuracy(x + .1, xList.length, xList, fxList, splain(x, xList.length, xList, fxList)));
-}
+let h = (b - a);
 
-
-function laGrange(x, n, xArr, fxArr) {
-    let ln = 0.;
-    for (let i = 0; i < n; i++) {
-        let l = 1.;
-        for (let j = 0; j < n; j++) {
-            if (i != j) {
-                l *= (x - xArr[j]) / (xArr[i] - xArr[j]);
-            }
-        }
-        ln += fxArr[i] * l;
-    }
-
-    return ln;
-}
-
-function eitken(x, n, xArr, fxArr) {
-    let P = [];
-    for (let i = 0; i < n; i++) P[i] = fxArr[i];
-    for (let j = 0; j < n - 1; j++) {
-        for (let i = j + 1; i < n; i++) {
-            P[i] = ((x - xArr[j]) * P[i] - (x - xArr[i]) * P[j]) / (xArr[i] - xArr[j]);
-        }
-    }
-    return P[P.length - 1];
-}
-
-function splain(x, n, xArr, fxArr) {
-    let Y = 0.;
-    for (let i = 0; i < n; i++) {
-        if (x < xArr[i] || (x == xArr[i] && i != 0)) {
-            let a = 1.0 * (fxArr[i] - fxArr[i - 1]) / (xArr[i] - xArr[i - 1]);
-            let b = fxArr[i - 1] - a * xArr[i - 1];
-            Y = a * x + b;
+for (; ;) {
+    x = a + (h / 2);
+    s = sign(x);
+    if (s === 0) {
+        break;
+    } else {
+        b = x;
+        h = x - a;
+        if (h < e) {
             break;
         }
     }
-    return Y;
+}
+console.log("x = " + x);
+
+function Fx(x) {
+    return 15*Math.pow(x, 4) - 3*x*x;
 }
 
-function inaccuracy(x, n, xArr, fxArr, F) {
-    let w = 1;
-    for (let i = 0; i < n; i++) {
-        w = w * (x - xArr[i]);
-    }
-    let R = F / w;
-    let xNew = [];
-    xNew.push(x);
-    for (let i = 0; i < n; i++) {
-        xNew.push(xArr[i]);
-    }
-    for (let i = 1; i < xNew.length; i++) {
-        let v = 1;
-        for (let j = 0; j < xNew.length; j++) {
-            if (j != i) {
-                v = v * (xNew[i] - xNew[j]);
-            }
-        }
-        R = R + (fxArr[i - 1] / v);
-    }
-    return Math.abs(w * R);
+function fx(x) {
+    return 3*Math.pow(x, 5) - Math.pow(x, 3) + 12;
 }
+
+function sign(x) {
+    let res = Fx(x);
+    if (res === 0) return 0;
+    else if (res > 0) return 1;
+    else return -1;
+}
+
+
